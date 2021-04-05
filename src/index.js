c45262a5d94e17e1d5df8eca3af1c2de55a7816f
Alexandr Antonenko <a.antonenko@corp.mail.ru>
2021-04-05 20:16:26 +0300

    render canvas with pixi

--- src/index.js
+++ src/index.js
@@ -5,5 +5,9 @@ if (!PIXI.utils.isWebGLSupported()) {
     type = 'canvas';
 }
 
-PIXI.utils.sayHello(type);
-console.log('***')
+//Create a Pixi Application
+const app = new PIXI.Application({width: 256, height: 256});
+app.renderer.backgroundColor = 0x061639;
+
+//Add the canvas that Pixi automatically created for you to the HTML document
+document.body.appendChild(app.view);
