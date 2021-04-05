e9db4d420fe8e3736935a22b6ffe6b17bcb08620
Alexandr Antonenko <a.antonenko@corp.mail.ru>
2021-04-05 20:56:22 +0300

    added sprite in stage

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import * as PIXI from 'pixi.js';
+import Cat from '../images/cat.png';
 
 let type = 'WebGL';
 if (!PIXI.utils.isWebGLSupported()) {
@@ -6,8 +7,26 @@ if (!PIXI.utils.isWebGLSupported()) {
 }
 
 //Create a Pixi Application
-const app = new PIXI.Application({width: 256, height: 256});
-app.renderer.backgroundColor = 0x061639;
+const app = new PIXI.Application({
+    width: 256, 
+    height: 256,
+    antialias: true,
+    resolution: 1,
+});
 
 //Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
+
+const stage = app.stage;
+const imagePath = Cat;
+const loader = new PIXI.Loader();
+loader
+.add(imagePath)
+.load(setup)
+
+function setup() {
+    const cat = new PIXI.Sprite(
+        loader.resources[imagePath].texture
+    );
+    stage.addChild(cat);
+}
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,14 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     sourceMapFilename: "[name].js.map"
   },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  },
   devtool: "source-map",
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
